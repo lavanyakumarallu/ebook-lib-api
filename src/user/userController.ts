a2286a8901dd8323b22ec85abc93a8ba96b43254
lavanyakumarallu <allulucky27@gmail.com>
2024-10-27 14:42:46 +0530

    Feat: Add User model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,11 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import createHttpError from 'http-errors';
+import userModel from './userModel';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
   // validation
   if (!name || !email || !password) {
     const error = createHttpError(400, 'All fields are required');
+    return next(error);
+  }
+  // Database call
+  const user = await userModel.findOne({ email });
+
+  if (user) {
+    const error = createHttpError(400, 'User already exists with this email.');
     return next(error);
   }
   // process
