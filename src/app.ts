c29141bd88970cf20e6c55c267406f36674b59b8
lavanyakumarallu <allulucky27@gmail.com>
2024-10-27 14:31:54 +0530

    Feat: Add user register validation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express from 'express';
-import createHttpError from 'http-errors';
 import globelErrorhandler from './middlewares/globalErrorHandler';
 import userRouter from './user/userRouter';
 
 const app = express();
+app.use(express.json());
 
 // Routes
 app.get('/', (_req, res, next) => {
