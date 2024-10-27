2e98e6bc2ea7e2f8270068fa0eed2a6b8386708c
lavanyakumarallu <allulucky27@gmail.com>
2024-10-27 18:45:40 +0530

    Feat: Setup "multer" package

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -6,12 +6,27 @@ import {
   updateBook,
   deleteBook,
 } from './bookController';
+import multer from 'multer';
+import path from 'path';
 
 const bookRouter = express.Router();
 
+// File Store Local -> Cloud -> Delete Local
+const upload = multer({
+  dest: path.resolve(__dirname, '../../public/data/uploads'),
+  limits: { fieldSize: 3e7 }, // 30mb
+});
+
 // Routes
 bookRouter.get('/', getBookList);
-bookRouter.post('/', createBook);
+bookRouter.post(
+  '/',
+  upload.fields([
+    { name: 'coverImage', maxCount: 1 },
+    { name: 'file', maxCount: 1 },
+  ]),
+  createBook
+);
 bookRouter.get('/{id}', getBook);
 bookRouter.put('/{id}', updateBook);
 bookRouter.delete('/{id}', deleteBook);
