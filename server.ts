9631a00d05138f970489050f86fa7f0d0ba9f6dc
lavanyakumarallu <allulucky27@gmail.com>
2024-10-27 11:24:40 +0530

    Feat: Setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log('Welocome to ebook apis.')
+import app from './src/app';
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listing on port: ${port}`);
+  });
+};
+
+startServer();
