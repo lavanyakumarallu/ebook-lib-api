707d16bd194dbceb84b46a93cf3f6fb77f658b03
lavanyakumarallu <allulucky27@gmail.com>
2024-10-27 18:05:32 +0530

    Feat: Change create user status code to 201

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -49,7 +49,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       expiresIn: '7d',
     });
     // response
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (error) {
     return next(createHttpError(500, 'Error while signing the jwt token'));
   }
