dee6a127419ea7320c7b025705cc6eb114a126ec
lavanyakumarallu <allulucky27@gmail.com>
2024-10-29 11:50:45 +0530

    Feat: Add CORS support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import express from 'express';
+import cors from 'cors';
 import globelErrorhandler from './middlewares/globalErrorHandler';
 import userRouter from './user/userRouter';
 import bookRouter from './book/bookRouter';
+import { config } from './config/config';
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain as string,
+  })
+);
 app.use(express.json());
 
 // Routes
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,8 +3,9 @@ conf();
 
 const _config = {
   port: process.env.PORT,
-  databaseUrl: process.env.MONGO_URI,
   env: process.env.NODE_ENV,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
+  databaseUrl: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
